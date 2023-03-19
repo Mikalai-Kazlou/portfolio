0811409ccc72fadfc14f7e6f3cab6d0ecb1806a7
Mikalai Kazlou <mikalai_kazlou@epam.com>
2023-03-19 10:51:01 +0300

    feat: add 2 new projects

--- modules/projects.js
+++ modules/projects.js
@@ -12,6 +12,18 @@ const projects = [
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
   },
   {
+    title: "Gem-puzzle",
+    link: "https://mikalai-kazlou.github.io/gem-puzzle/",
+    image: "./images/gem-puzzle.jpg",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    title: "Songbird",
+    link: "https://mikalai-kazlou.github.io/songbird/pages/quiz/",
+    image: "./images/songbird.jpg",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
     title: "Online-store",
     link: "https://mikalai-kazlou.github.io/online-store/",
     image: "./images/online-store.jpg",
