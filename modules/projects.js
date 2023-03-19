c5de65253021c7c479a0615d3426ccdf5c80daed
Mikalai Kazlou <mikalai_kazlou@epam.com>
2023-03-19 09:37:31 +0300

    feat: add template for projects

---
+++ modules/projects.js
@@ -0,0 +1,22 @@
+const projects = [
+  {
+    title: "Travel",
+    link: "https://mikalai-kazlou.github.io/travel/",
+    image: "./images/travel.jpg",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    title: "Momentum",
+    link: "https://mikalai-kazlou.github.io/momentum/",
+    image: "./images/momentum.jpg",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    title: "Online-store",
+    link: "https://mikalai-kazlou.github.io/online-store/",
+    image: "./images/online-store.jpg",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  }
+];
+
+export default projects;
---
+++ script.js
@@ -0,0 +1,49 @@
+import projects from "./modules/projects.js";
+
+function fillProject(uiProject, project) {
+  let uiElement;
+
+  const loadImage = (src, alt, uiImage) => {
+    const image = new Image();
+    image.src = src;
+    image.onload = () => {
+      uiImage.setAttribute("src", src);
+      uiImage.setAttribute("alt", alt);
+    };
+    image.onerror = () => {
+      setTimeout(() => loadImage(src, alt, uiImage), 1000);
+    };
+  };
+
+  const getClassName = (element) => {
+    return `.project__${element}`;
+  }
+
+  const uiImage = uiProject.querySelector(getClassName("image"));
+  loadImage(project.image, project.title, uiImage);
+
+  uiElement = uiProject.querySelector(getClassName("title"));
+  uiElement.textContent = project.title;
+
+  uiElement = uiProject.querySelector(getClassName("link"));
+  uiElement.href = project.link;
+
+  uiElement = uiProject.querySelector(getClassName("description"));
+  uiElement.innerHTML = project.description;
+}
+
+function fillProjects() {
+  const uiTemplate = document.querySelector("#project-template");
+  const uiFragment = document.createDocumentFragment();
+
+  projects.forEach((project) => {
+    const uiProject = uiTemplate.content.cloneNode(true);
+    fillProject(uiProject, project);
+    uiFragment.append(uiProject);
+  });
+
+  const uiProjects = document.querySelector(".projects");
+  uiProjects.append(uiFragment);
+}
+
+fillProjects();
